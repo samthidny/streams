a7a5965332753b4047f825e6185042abb6f22d88
Andy Smith <asmit844@ford.com>
2024-02-19 23:23:51 +0000

    tidy up

---
+++ src/pages/Favourites.tsx
@@ -0,0 +1,22 @@
+import useFavourites from '../hooks/useFavourites';
+import './Favourites.css';
+import ReelCardLoader from '../components/ReelCardLoader';
+import { ITitle } from '../data/ITitle';
+
+export function Favourites() {
+
+    const [favourites, removeFavourite] = useFavourites();
+
+    function removeHandler(id: string) {
+        removeFavourite({ id });
+    }
+
+    const cardList = favourites.map((record: ITitle) => ({ id: record.id, original_title: record.title })).map((record: ITitle, index: number) => <div key={index}><ReelCardLoader key={record.id} title={record}></ReelCardLoader><button className="favourites-remove-button" onClick={() => removeHandler(record.id)}>REMOVE</button></div>)
+
+    return <div className="favourites">
+        <h1>Favourites</h1>
+        <div className="favourites-list">
+            {cardList}
+        </div>
+    </div>
+}
